3263c309af99120b82d468cf5fde11f19ef08000
Patrick Michaud <pmichaud@Patrick-Michauds-MacBook-Pro-2.local>
2012-03-09 18:10:42 -0800

    Making everything rotate

---
+++ SpotSeeker/ViewControllers/RotatingTabBarController.h
@@ -0,0 +1,13 @@
+//
+//  RotatingTabBarController.h
+//  SpotSeeker
+//
+//  Created by Patrick Michaud on 3/9/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RotatingTabBarController : UITabBarController
+
+@end
---
+++ SpotSeeker/ViewControllers/RotatingTabBarController.m
@@ -0,0 +1,43 @@
+//
+//  RotatingTabBarController.m
+//  SpotSeeker
+//
+//  Created by Patrick Michaud on 3/9/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "RotatingTabBarController.h"
+
+@interface RotatingTabBarController ()
+
+@end
+
+@implementation RotatingTabBarController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
