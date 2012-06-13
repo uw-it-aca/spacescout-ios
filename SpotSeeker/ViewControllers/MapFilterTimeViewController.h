83c705653d7d441dcb321883c1168153ccb0181b
Patrick Michaud <pmichaud@uw.edu>
2012-06-12 17:02:27 -0700

    Rough start on the hours picker.

--- SpotSeeker/ViewControllers/MapFilterTimeViewController.h
+++ SpotSeeker/ViewControllers/MapFilterTimeViewController.h
@@ -8,15 +8,15 @@
 
 #import <UIKit/UIKit.h>
 
-@interface MapFilterTimeViewController : UIViewController {
-    IBOutlet UIDatePicker *date_picker;
+@interface MapFilterTimeViewController : UIViewController <UIPickerViewDelegate, UIPickerViewDataSource> {
+    IBOutlet UIPickerView *time_picker;
     NSMutableDictionary *filter;
 
 }
 
 -(IBAction)timeSelected:(id)sender;
 
-@property (nonatomic, retain) UIDatePicker *date_picker;
+@property (nonatomic, retain) UIPickerView *time_picker;
 @property (nonatomic, retain) NSMutableDictionary *filter;
 
 @end
--- SpotSeeker/ViewControllers/MapFilterTimeViewController.m
+++ SpotSeeker/ViewControllers/MapFilterTimeViewController.m
@@ -11,11 +11,11 @@
 
 @implementation MapFilterTimeViewController
 
-@synthesize date_picker;
+@synthesize time_picker;
 @synthesize filter;
 
 -(IBAction)timeSelected:(id)sender {
-    [self.filter setObject:self.date_picker.date forKey:@"selected_date"];
+//    [self.filter setObject:self.date_picker.date forKey:@"selected_date"];
 }
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
@@ -33,7 +33,7 @@
 	// Do any additional setup after loading the view.
     NSDate *selected_date = [self.filter objectForKey:@"selected_date"];
     if (selected_date != Nil) {
-        [self.date_picker setDate:selected_date];
+//        [self.date_picker setDate:selected_date];
     }
 }
 
@@ -46,6 +46,66 @@
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
 {
     return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+#pragma mark -
+#pragma mark picker methods
+
+-(NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView {
+    return 4;
+}
+
+-(NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component {
+    switch (component) {
+        case 0:
+            return 7;
+        case 1:
+            return 12;
+        case 2:
+            return 4;
+        case 3:
+            return 2;
+        default:
+            return -1;
+    }
+}
+
+
+
+-(CGFloat)pickerView:(UIPickerView *)pickerView widthForComponent:(NSInteger)component {
+    switch (component) {
+        case 0:
+            return 140;
+        case 1:
+            return 45.0;
+        case 2:
+            return 45.0;
+        default:
+            return 60.0;
+    }
+}
+
+-(NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component {
+    if (component == 3) {
+        switch (row) {
+            case 0:
+                return @"AM";
+            case 1:
+                return @"PM";
+        }
+    }
+    else if (component == 2) {
+        return [NSString stringWithFormat:@"%i", row * 15];
+    }
+    else if (component == 1) {
+        return [NSString stringWithFormat:@"%i", row+1];
+    }
+    else {
+        NSArray *days = [NSArray arrayWithObjects:@"Monday", @"Tuesday", @"Wednesday", @"Thursday", @"Friday", @"Saturday", @"Sunday", nil];
+        return [days objectAtIndex:row];
+    }
+    
+    return @"OK";
 }
 
 @end
