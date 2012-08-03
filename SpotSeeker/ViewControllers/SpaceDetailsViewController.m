77a5afe590f4fea9f7321d9d03e16cceff43af79
Craig M. Stimmel <cstimmel@uw.edu>
2012-08-03 15:20:02 -0700

    Strip out some characters google maps doesn't like when we're passing it a location to give directions to. Fixes SPOT-246.

--- SpotSeeker/ViewControllers/SpaceDetailsViewController.m
+++ SpotSeeker/ViewControllers/SpaceDetailsViewController.m
@@ -268,8 +268,15 @@
             // ...
         }
         else if (indexPath.row - offset == 1) {
-            UIApplication *app = [UIApplication sharedApplication];  
-            NSString *url = [NSString stringWithFormat:@"http://maps.google.com/maps?saddr=Current+Location&daddr=%f,%f%%20(%@)", [self.spot.latitude floatValue], [self.spot.longitude floatValue], [self.spot.name stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding]];
+            UIApplication *app = [UIApplication sharedApplication];
+
+            // Google Maps fails to give good directions for spotnames with (,),& in them
+            NSString *fixed_spotname = [self.spot.name stringByAddingPercentEscapesUsingEncoding:NSASCIIStringEncoding];
+            fixed_spotname = [fixed_spotname stringByReplacingOccurrencesOfString:@"(" withString:@""];
+            fixed_spotname = [fixed_spotname stringByReplacingOccurrencesOfString:@")" withString:@""];
+            fixed_spotname = [fixed_spotname stringByReplacingOccurrencesOfString:@"&" withString:@""];
+
+            NSString *url = [NSString stringWithFormat:@"http://maps.google.com/maps?saddr=Current+Location&daddr=%f,%f%%20(%@)", [self.spot.latitude floatValue], [self.spot.longitude floatValue], fixed_spotname];
             [app openURL:[NSURL URLWithString:url]];  
         }
     }
