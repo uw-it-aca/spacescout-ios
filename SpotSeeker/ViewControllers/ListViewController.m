33f809ea84946b5d90af3b203e938dfd0d4cd94f
Craig M. Stimmel <cstimmel@uw.edu>
2012-08-21 14:58:13 -0700

    Run a search for spots again if we get to the list view and no search has completed. Fixes SPOT-191.

--- SpotSeeker/ViewControllers/ListViewController.m
+++ SpotSeeker/ViewControllers/ListViewController.m
@@ -31,6 +31,9 @@
     [super viewDidLoad];
     self.rest = [[REST alloc] init];
     self.rest.delegate = self;
+    if (self.current_spots.count < 1) {
+        [self runSearch];
+    }
     [self sortSpots];
     [self.spot_table reloadData];
     self.requests = [[NSMutableDictionary alloc] init];
