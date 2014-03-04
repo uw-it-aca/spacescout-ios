3dcf160f53a6d20428e26fc094bec27cc9d629a4
Patrick Michaud <pmichaud@uw.edu>
2014-03-03 22:17:38 -0800

    Using apple’s light effect instead of a gaussian - way faster.

--- SpotSeeker/Widgets/SideMenu.m
+++ SpotSeeker/Widgets/SideMenu.m
@@ -7,6 +7,7 @@
 //
 
 #import "SideMenu.h"
+#import "UIImage+ImageEffects.h"
 
 @implementation SideMenu
 @synthesize navigation_menu_view;
@@ -23,26 +24,8 @@
 }
 
 -(UIImage *)getBlurredImageFromUIImage:(UIImage *)image {
-    // Taken from http://stackoverflow.com/questions/17041669/creating-a-blurring-overlay-view
-    
-    // create our blurred image
-    CIContext *context = [CIContext contextWithOptions:nil];
-    CIImage *inputImage = [CIImage imageWithCGImage:image.CGImage];
-    
-    // setting up Gaussian Blur (we could use one of many filters offered by Core Image)
-    CIFilter *filter = [CIFilter filterWithName:@"CIGaussianBlur"];
-    [filter setValue:inputImage forKey:kCIInputImageKey];
-    [filter setValue:[NSNumber numberWithFloat:15.0f] forKey:@"inputRadius"];
-    CIImage *result = [filter valueForKey:kCIOutputImageKey];
-    
-    // CIGaussianBlur has a tendency to shrink the image a little,
-    // this ensures it matches up exactly to the bounds of our original image
-    CGImageRef cgImage = [context createCGImage:result fromRect:[inputImage extent]];
-    
-    UIImage *blurred_image = [UIImage imageWithCGImage:cgImage];//create a UIImage for this function to "return" so that ARC can manage the memory of the blur... ARC can't manage CGImageRefs so we need to release it before this function "returns" and ends.
-    CGImageRelease(cgImage);//release CGImageRef because ARC doesn't manage this on its own.
-
-    return blurred_image;
+    UIImage *blurredSnapshotImage = [image applyLightEffect];
+    return blurredSnapshotImage;
 }
 
 -(void) buildViews {
