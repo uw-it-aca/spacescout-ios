6bfdc9263d86f8bed62d345daff6a18b6430942f
pmichaud <pmichaud@pmichauds-MacBook-Pro.local>
2014-04-18 11:11:45 -0700

    Write review colors and sizes.

--- SpotSeeker/ViewControllers/ReviewSpaceViewController.m
+++ SpotSeeker/ViewControllers/ReviewSpaceViewController.m
@@ -248,9 +248,11 @@ NSString *UNSELECTED_IMAGE = @"StarRating-big_blank";
     UIButton *submit = (UIButton *)[self.view viewWithTag:300];
     if ([self hasValidReview]) {
         submit.enabled = TRUE;
+        submit.backgroundColor = [UIColor colorWithRed:81.0/255.0 green:77.0/255.0 blue:163.0/255.0 alpha:1.0];
     }
     else {
         submit.enabled = FALSE;
+        submit.backgroundColor = [UIColor colorWithRed:204.0/255.0 green:204.0/255.0 blue:204.0/255.0 alpha:1.0];
     }
 }
 
